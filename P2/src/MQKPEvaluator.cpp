1ea8baf88d4e561c25ebe3012def418a51af2eac
Adoen Lunnae <unshekdelmonton@gmail.com>
2020-03-05 10:28:28 +0100

    Completed local search for MQKP

---
+++ P2/src/MQKPEvaluator.cpp
@@ -0,0 +1,92 @@
+/*
+ * MQKPEvaluator.cpp
+ *
+ * File defining the methods of the class MQKPEvaluator.
+ *
+ * It is a part of the skeleton code provided for the MQKP, as a part of the module Metaheuristics of the
+ * "Grado de Ingeniería Informática" of the University of Córdoba (Spain).
+ *
+ * @author Carlos García cgarcia@uco.es
+ */
+
+#include <MQKPEvaluator.h>
+#include <MQKPInstance.h>
+#include <MQKPSolution.h>
+
+unsigned MQKPEvaluator::_numEvaluations = 0;
+
+double MQKPEvaluator::computeFitness(MQKPInstance& instance, MQKPSolution& solution)
+{
+
+    double fitness = 0;
+
+    /*
+	 * Calculate the fitness of the solution in the following way:
+	 *   1. Obtain the maximum violation of the knapsack capacities, by using the corresponding function.
+	 *   2. If it is higher than 0, then return the maximum violation multiplied by -1.
+	 *   3. If not, the return the sum of the individual and share profits using the corresponding function.
+	 */
+    if (instance.getMaxCapacityViolation(solution) > 0) {
+        fitness = instance.getMaxCapacityViolation(solution) * (-1);
+    } else {
+        fitness = instance.getSumProfits(solution);
+    }
+
+    _numEvaluations++;
+    return fitness;
+}
+
+double MQKPEvaluator::computeDeltaFitness(MQKPInstance& instance,
+    MQKPSolution& solution, int indexObject, int indexKnapsack)
+{
+
+    _numEvaluations++;
+
+    /**
+	 * 
+	 * Given that the fitness depends on the violation of the capacities of the knapsack,
+	 * you should obtain current (maximum) violation and the possible new violation, together
+	 * with the possible change in the sum of profits.
+	 *
+	 * 1. Obtain maximum current violation
+	 * 2. Call to MQKPInstance.getDeltaMaxCapacityViolation to return how the maximum violation is modified
+	 *    after the operation
+	 * 3. Sum the two previous quantities to obtain maximum violation after applying the operation
+	 * 4. Obtain the current sum of profits
+	 * 5. Call to MQKPInstance.getDeltaSumProfits to know how the sum of profits is going to change if the
+	 *    operation is performed.
+	 * 6. Sum the two previous quantities to obtain the sum of profits if the operation is performed.
+	 *
+	 * Finally
+	 * - If current and new violations are positive, return the negative of deltaMaxCapacityViolation
+	 * - If both violations are 0, return deltaSumProfits
+	 * - If only current violation is positive and the new one is 0, return the sum of newSumProfits
+	 *   plus the negative of deltaMaxCapacityViolation
+	 * - If only the new violation is positive, return the negative of
+	 *    (the sum of current profits + the new violation of capacities)
+	 */
+
+    double currentViolation = instance.getMaxCapacityViolation(solution);
+    double deltaViolation = instance.getDeltaMaxCapacityViolation(solution, indexObject, indexKnapsack);
+    double newMaxViolation = currentViolation + deltaViolation;
+
+    double currentProfit = instance.getSumProfits(solution);
+    double deltaProfit = instance.getDeltaSumProfits(solution, indexObject, indexKnapsack);
+    double newProfit = currentProfit + deltaProfit;
+
+    double deltaFitness = 0;
+
+    if (currentViolation > 0 && newMaxViolation > 0)
+        deltaFitness = -deltaViolation;
+    else if (currentViolation > 0)
+        deltaFitness = newProfit - deltaViolation;
+    else if (newMaxViolation > 0)
+        deltaFitness = -(newProfit + newMaxViolation);
+    else
+        return deltaProfit;
+}
+
+void MQKPEvaluator::resetNumEvaluations()
+{
+    _numEvaluations = 0;
+}
---
+++ P2/src/MQKPSimpleBestImprovementNO.cpp
@@ -0,0 +1,66 @@
+/*
+ * MQKPSimpleBestImprovementNO.cpp
+ *
+ * File defining the methods of the class MQKPSimpleBestImprovementNO.
+ *
+ * It is a part of the skeleton code provided for the MQKP, as a part of the module Metaheuristics of the
+ * "Grado de Ingeniería Informática" of the University of Córdoba (Spain).
+ *
+ * @author Carlos García cgarcia@uco.es
+ */
+
+#include <MQKPEvaluator.h>
+#include <MQKPInstance.h>
+#include <MQKPObjectAssignmentOperation.h>
+#include <MQKPSimpleBestImprovementNO.h>
+#include <MQKPSolution.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+bool MQKPSimpleBestImprovementNO::findOperation(MQKPInstance& instance,
+    MQKPSolution& solution, MQKPChangeOperation& operation)
+{
+
+    MQKPObjectAssignmentOperation* oaOperation = dynamic_cast<MQKPObjectAssignmentOperation*>(&operation);
+    if (oaOperation == NULL) {
+        cerr << "MQKPSimpleBestImprovementNO::findOperation received an operation object of a class different from MQKPObjectAssignmentOperation" << endl;
+        exit(1);
+    }
+
+    //Create a permutation of the indices of the objects and initialize some variables
+    vector<int> perm;
+    int numObjs = instance.getNumObjs();
+    MQKPInstance::randomPermutation(numObjs, perm);
+    int numKnapsacks = instance.getNumKnapsacks();
+    bool initialised = false;
+    double bestDeltaFitness = 0;
+
+    /* 
+	 * 1. For each object of the problem (iterating them in the order defined by perm)
+	 *   a. For each knapsack of the problem (Note: do not forget any of them)
+	 *     i. Obtain the value deltaFitness obtained from assign this object to the knapsack in the solution
+	 *
+	 *     ii. If deltaFitness is better than bestDeltaFitness or if bestDeltaFitness was not initialised (initialised == false)
+	 *       . Set initialised to true
+	 *       . Update bestDeltaFitness
+	 *       . Update the values of the operation in oaOperation
+	 *
+	 * 2. Finally, return true if bestDeltaFitness is positive and false otherwise
+	 *
+	 */
+    double deltaFitness;
+
+    for (int indexObject : perm) {
+        for (int indexKnapsack = 0; indexKnapsack < numKnapsacks; ++indexKnapsack) {
+            deltaFitness = MQKPEvaluator::computeDeltaFitness(instance, solution, indexObject, indexKnapsack);
+
+            if (deltaFitness > bestDeltaFitness || !initialised) {
+                initialised = true;
+                bestDeltaFitness = deltaFitness;
+                oaOperation->setValues(indexObject, indexKnapsack, bestDeltaFitness);
+            }
+        }
+    }
+    return bestDeltaFitness > 0;
+}
